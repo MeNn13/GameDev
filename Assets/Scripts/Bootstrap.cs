63418d64ba39e6de286004d989fd8005abe01dea
Khasan000 <zkurycin@gmail.com>
2023-09-22 12:41:04 +0300

    Добавление бутстрапа и добавление новой LoadScene

---
+++ Assets/Scripts/Bootstrap.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bootstrap : MonoBehaviour
+{
+   // Init all Services
+   private void Awake()
+   {
+      
+   }
+}

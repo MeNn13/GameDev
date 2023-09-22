02008778bc5872660936d381e1530070426dcfe4
MeNn13 <banzai.davai@gmail.com>
2023-09-22 15:58:04 +0300

    Update Game

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,13 @@
+using System.Net.Http.Headers;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Attributes")]
     [SerializeField] private float speed = 7f;
+
+    [Header("Unity Setup reference")]
+    [SerializeField] private SpriteRenderer sprite;
 
     void Update()
     {
@@ -15,5 +20,10 @@ public class Player : MonoBehaviour
         float moveY = Input.GetAxis("Vertical");
 
         transform.position += new Vector3(moveX, moveY, 0) * speed * Time.deltaTime;
+
+        if (moveX > .1f)
+            sprite.flipX = false;
+        else
+            sprite.flipX = true;
     }
 }
